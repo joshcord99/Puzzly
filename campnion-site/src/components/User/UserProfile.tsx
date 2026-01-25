a33923bcfecf08053fce5072d4c843322f4dee8b
Joshua Cordial <joshcord99@gmail.com>
2026-01-24 23:54:34 -0800

    removed userid and passed props to userprofile

--- campnion-site/src/components/User/UserProfile.tsx
+++ campnion-site/src/components/User/UserProfile.tsx
@@ -1,3 +1,11 @@
-export default function UserProfile() {
+import { UserProfile as UserProfileType } from '@/utils/types';
+
+interface UserProfileProps {
+  user: UserProfileType;
+  onEdit?: () => void;
+  onDelete?: () => void;
+}
+
+export default function UserProfile({ user, onEdit, onDelete }: UserProfileProps) {
   return null
 }
