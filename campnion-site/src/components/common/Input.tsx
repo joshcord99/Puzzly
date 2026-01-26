2a42c6750a64622e393ef14b59b9c43a6840ddce
Joshua Cordial <joshcord99@gmail.com>
2026-01-25 23:59:20 -0800

    added tpyes to input

--- campnion-site/src/components/common/Input.tsx
+++ campnion-site/src/components/common/Input.tsx
@@ -1,3 +1,11 @@
-export default function Input() {
-  return null
+import { InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+  helperText?: string;
+}
+
+export default function Input(props: InputProps) {
+  return null;
 }
